63b5283c1d1f8c7fc502aaa09b21a44f4bcecceb
Whacky Ghost <nischeysingh@gmail.com>
2026-01-16 14:27:53 +0530

    Prime code

---
+++ DSA_LAB_ASSIGNMENT_1/question_4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+    int input;
+    cout << "Enter a Integer: ";
+    cin >> input;
+    // (input % 2) == 0 ? cout << "Even" : cout << "Odd";
+
+    for (int i = 2; i <= sqrt(input); i++)
+    {
+        if (input % i == 0)
+        {
+            cout << "It is Composite: " << i;
+            return 0;
+        }
+        
+    }
+    cout << "It is Prime";
+    return 0;
+}
