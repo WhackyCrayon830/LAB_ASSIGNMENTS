fc19819da60b0fc7230b33eabf28d289ad902fff
Whacky Ghost <nischeysingh@gmail.com>
2026-02-03 14:24:12 +0530

    updates

--- DSA_ASSIGNMENTS/Nishchey_1024150237/LAB_2/Nishchey-1024150237_Question_3_a.cpp
+++ DSA_ASSIGNMENTS/Nishchey_1024150237/LAB_2/Nishchey-1024150237_Question_3_a.cpp
@@ -1,3 +1,13 @@
+/*
+Name           : Nishchey Singh
+Roll Number    : 1024150237
+Question       : Pattern Printing Questions with a input of 5;
+Acknowledgment : https://www.w3schools.com/cpp/cpp_ref_reference.asp,
+                 https://www.geeksforgeeks.org/cpp/c-plus-plus/,
+                 https://www.udemy.com/course/datastructurescncpp/
+                 
+*/
+
 #include <iostream>
 #include <cstring>
 using namespace std;
