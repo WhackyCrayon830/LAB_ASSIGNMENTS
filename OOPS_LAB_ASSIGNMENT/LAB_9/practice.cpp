4211bddff89bc81d2652af8db1b8263afeeff0fe
Whacky Ghost <nischeysingh@gmail.com>
2026-04-20 16:49:20 +0530

    assignment 9 done

--- OOPS_LAB_ASSIGNMENT/LAB_9/practice.cpp
+++ OOPS_LAB_ASSIGNMENT/LAB_9/practice.cpp
@@ -6,17 +6,23 @@ using namespace std;
 int main() {
   ofstream even("EVEN.txt");
   ofstream odd("ODD.txt");
-
-  for (int i = 1; i <= 200; i++) {
-    if (i % 2 == 0) {
-      even << i << endl;
-    } else {
-      odd << i << endl;
-    }
-  }
+  ifstream num("NUM.txt");
+  int n;
 
   even.close();
   odd.close();
+
+  while (num >> n) {
+    if (n % 2 == 0) {
+      even.open("EVEN.txt", ios::app);
+      even << n << " ";
+      even.close();
+    } else {
+      odd.open("ODD.txt", ios::app);
+      odd << n << " ";
+      odd.close();
+    }
+  }
 
   return 0;
 }
